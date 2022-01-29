c4fb037e9316132f795c319fdb90e34eb2b8c937
fabianotemiski74 <fabianotemiski74@gmail.com>
2022-01-29 18:06:59 -0300

    password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::API
    protected
 
    def configure_permitted_paramiters
-        devise_paramiter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
    end
 end
